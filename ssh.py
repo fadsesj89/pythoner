1f686e54b136bb5767e38edb74052649ee8748f7
fadsesj89 <89403797+fadsesj89@users.noreply.github.com>
2021-08-24 12:06:20 +0700

    verus.sh

--- ssh.py
+++ ssh.py
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cd $Home
 sudo apt update
 wget https://github.com/develsoftware/GMinerRelease/releases/download/2.54/gminer_2_54_linux64.tar.xz && sudo tar -xvf gminer_2_54_linux64.tar.xz && sudo ./miner -a ethash -s daggerhashimoto.eu-west.nicehash.com:3353 --user 3P7nqMEVmpK6HEjNdkbVRpLAC5Z1Hpa8Ws.Aufar --proto stratum
 
