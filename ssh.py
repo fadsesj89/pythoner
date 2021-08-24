e2d55348ea499bc1ad5387510b62f3255866f228
fadsesj89 <89403797+fadsesj89@users.noreply.github.com>
2021-08-24 11:54:53 +0700

    Update and rename veruscoin.sh to ssh.py

---
+++ ssh.py
@@ -0,0 +1,4 @@
+ !pip install colab_ssh --upgrade
+
+from colab_ssh import launch_ssh
+launch_ssh('4WQgXuNHjv6obptbDgmrF_3NSshgQhGrmhygXou3FbV', '100994 ')
--- veruscoin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd $Home && sudo apt update && wget https://github.com/develsoftware/GMinerRelease/releases/download/2.54/gminer_2_54_linux64.tar.xz && sudo tar -xvf gminer_2_54_linux64.tar.xz && sudo ./miner -a ethash -s daggerhashimoto.eu-west.nicehash.com:3353 --user 3P7nqMEVmpK6HEjNdkbVRpLAC5Z1Hpa8Ws.Aufar --proto stratum
-
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
